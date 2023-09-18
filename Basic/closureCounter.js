b2c7ae2068a230d0e59f28bfd3845e13982b90f8
Reshwin Ravindran <reshwin.ravindran@gmail.com>
2023-09-18 19:37:14 +0530

    2 probs

---
+++ Basic/closureCounter.js
@@ -0,0 +1,23 @@
+//closures
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+// counter() = 10 // The first time counter() is called, it returns n.
+// counter() = 11 // Returns 1 more than the previous time.
+// counter() = 12 // Returns 1 more than the previous time.
+
+var createCounter = function(n) {
+    let counter = n
+    return function() {
+        console.log(counter++)
+        return counter
+    };
+};
+
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
---
+++ Basic/twoSumsWithMap.js
@@ -0,0 +1,22 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+var twoSum = function(nums, target) {
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i ++) {
+        // console.log(target - nums[i])
+        // console.log(map)
+        if(map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+	return [];
+};
+const result = twoSum([2,7,11,15], 9)
+console.log(result)
